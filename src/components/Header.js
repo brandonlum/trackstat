268338d32bfeb22a9cbb3665626700f7357dd462
brandonlum <brandon.lum09@gmail.com>
2019-08-10 02:08:14 -0400

    Setup Initial header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+
+class Header extends Component {
+    render () {
+        return (
+            <div>
+                <h1>Score Tracker - Track all your scoring in one convenient location!</h1>
+            </div>
+        )
+    }
+}
+
+
+export default Header
