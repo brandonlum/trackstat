7a7a769b0f6ebbc6cd02c025bcf47ddf43b9e228
brandonlum <brandon.lum09@gmail.com>
2019-08-10 11:36:39 -0400

    Updating some styling, adding bootstrap/reactstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react'
 
-
 class Header extends Component {
     render () {
         return (
             <div>
-                <h1>Score Tracker - Track all your scoring in one convenient location!</h1>
+                <header><h1>ScoreTracker</h1>Track all your scoring in one convenient location!</header>
             </div>
         )
     }
