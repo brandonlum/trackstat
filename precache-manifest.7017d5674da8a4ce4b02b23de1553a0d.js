d736df14d7b9844bdc9136d4012613f7a9471c4d
brandonlum <brandon.lum09@gmail.com>
2019-08-17 00:42:38 -0400

    Updates

--- precache-manifest.7017d5674da8a4ce4b02b23de1553a0d.js
+++ precache-manifest.7017d5674da8a4ce4b02b23de1553a0d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2e4bbb0357301fe3704e8bea1a445786",
+    "revision": "e54c424c851d6fe100cde835a8ef34ed",
     "url": "/trackstat-client/index.html"
   },
   {
-    "revision": "b2722b30456fb1b2cad2",
+    "revision": "2bf76979189e29cd2d30",
     "url": "/trackstat-client/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "4e43ad7fea1d004b7828",
+    "revision": "7e1e326891bd932decd4",
     "url": "/trackstat-client/static/css/main.67e25121.chunk.css"
   },
   {
-    "revision": "b2722b30456fb1b2cad2",
-    "url": "/trackstat-client/static/js/2.db6a7b19.chunk.js"
+    "revision": "2bf76979189e29cd2d30",
+    "url": "/trackstat-client/static/js/2.8bc1d9a2.chunk.js"
   },
   {
-    "revision": "4e43ad7fea1d004b7828",
-    "url": "/trackstat-client/static/js/main.a0ba9c02.chunk.js"
+    "revision": "7e1e326891bd932decd4",
+    "url": "/trackstat-client/static/js/main.69bcdf7a.chunk.js"
   },
   {
     "revision": "8efa725bf6107027ebe6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/trackstat-client/precache-manifest.1b4970acf994ff83756589d16ef6995c.js"
+  "/trackstat-client/precache-manifest.7017d5674da8a4ce4b02b23de1553a0d.js"
 );
 
 self.addEventListener('message', (event) => {
