9dcc16810f3b950489c8c5f53f1f23cde0d196fc
brandonlum <brandon.lum09@gmail.com>
2019-08-17 00:02:13 -0400

    Deploy front end

---
+++ build/precache-manifest.de8113dd921821e85c0a65e1a5791483.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c2c871be75705703a5e26422f403d9",
+    "url": "/trackstat-client/index.html"
+  },
+  {
+    "revision": "b2722b30456fb1b2cad2",
+    "url": "/trackstat-client/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "64d7e9b3942761a4a280",
+    "url": "/trackstat-client/static/css/main.67e25121.chunk.css"
+  },
+  {
+    "revision": "b2722b30456fb1b2cad2",
+    "url": "/trackstat-client/static/js/2.db6a7b19.chunk.js"
+  },
+  {
+    "revision": "64d7e9b3942761a4a280",
+    "url": "/trackstat-client/static/js/main.40f662f6.chunk.js"
+  },
+  {
+    "revision": "8efa725bf6107027ebe6",
+    "url": "/trackstat-client/static/js/runtime~main.f27e20bb.js"
+  }
+]);
